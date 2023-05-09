dae36369db9c1b0317522a890ef7d3e02ab09aca
domenicobacoli <domenico.bacoli89@gmail.com>
2023-05-09 16:47:08 +0200

    feat: add header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,70 @@
 <footer>
-    footer comics dc
+    <div class="footer-top">
+        <div class="container top">
+            <div class="footer-list">
+                <div class="col">
+                    <strong class="list-title">DC COMICS</strong>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>Tv</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <strong class="list-title">SHOP</strong>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <strong class="list-title">DC</strong>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy policy (New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <strong class="list-title">SITES</strong>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="footer-button">
+                    <button>SIGN-UP NOW!</button>
+                </div>
+                <div class="footer-social-link">
+                    <span>FOLLOW US</span>
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook icon">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter icon">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png')}}" alt="youtube icon">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest icon">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope icon">
+    
+                </div>
+            </div>
+        </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,25 @@
 <header>
-    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo comics dc">
-    <span>header comics dc</span>
+    <nav>
+        <div class="container">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo comics dc">
+            </div>   
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+            <div class="search">
+                <div class="text">Search</div>
+                <div class="search-icon"><i class="fa-solid fa-magnifying-glass"></i></div>
+            </div>
+        </div>
+    </nav>
 </header>
